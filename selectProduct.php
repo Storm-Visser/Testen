09488c1a916bfefc71f5162138d8ed8ddb658bca
Yannik Eisinga - Laptop <yannik.eisinga@student.nhlstenden.com>
2021-01-06 14:49:06 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Storm-Visser/Testen into main""
    This reverts commit 49b4507f57a9121aca2fe2135bb1881890085335.

---
+++ selectProduct.php
@@ -0,0 +1,50 @@
+<?php include 'header.php' ?>   
+    <div id="content">
+        <?php
+            include 'DBConnect.php';
+            $query = "SELECT * FROM producten";
+            echo "<table style='border:1px solid black'>
+                    <tr>
+                            <th>Product</th>
+                            <th>voeg toe<th>
+                    </tr>";
+            if(!$stmt = mysqli_prepare($conn, $query)) 
+            {
+                echo "Failed to prepare statement " , mysqli_error($conn);
+            }
+            else 
+            {
+                if(!mysqli_stmt_execute($stmt)) 
+                {
+                    echo "Failed to execute statement";
+                }
+                else 
+                {
+                    mysqli_stmt_bind_result($stmt, $ID, $productName);
+                    mysqli_stmt_store_result($stmt);
+                    while(mysqli_stmt_fetch($stmt)) 
+                    {
+                        echo "<tr>
+                        <td>$productName</td>
+                        <td><a href='addProduct.php?name=" . $productName . "'>Voeg toe</a></td>
+                        </tr>";
+                    }
+                }
+            }
+            echo "</table>";
+            if(!empty($_GET["R"]))
+            {
+                if($_GET["R"] == 1)
+                {
+                    echo "<p>Product toegevoegd aan boodschappenlijstje</p>";
+                }
+                else
+                {
+                    echo "<p>er is iets fout gegaan met het toevoegen van het product</p>";
+                }
+            }
+        ?>
+    </div>
+</body>
+
+</html>
