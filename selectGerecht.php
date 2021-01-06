96d3c9f58d5f1f6f77b4b9dc364084a7a8fb5a2a
RaamyAfifi <56026512+RaamyAfifi@users.noreply.github.com>
2021-01-06 12:11:32 +0100

    Create selectGerecht.php

---
+++ selectGerecht.php
@@ -0,0 +1,8 @@
+<form>
+    <p>U kunt hier kiezen voor een gerecht</p>
+            <select>
+                <option value="disabled"> Geen gerecht</option>
+                <option value="nasi"> Nasi </option>
+                <option value="lasagne"> Lasagne </option>
+            </select>
+        </form>
