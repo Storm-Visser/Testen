762223871c3e4ea38ce29b5c5ea3a9460487a615
TomOlijve <56630807+TomOlijve@users.noreply.github.com>
2021-01-07 18:51:02 +0100

    update

---
+++ deleteProduct.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>add a product</title>
+</head>
+
+<body>
+    <?php
+        include 'DBConnect.php';
+        $name = $_GET["name"];
+        $tableName = "boodschappenlijst";
+        $query = "DELETE FROM $tableName WHERE `product` = '$name'";
+        if(!$stmt = mysqli_prepare($conn, $query)) 
+        {
+            echo "Failed to prepare statement " , mysqli_error($conn);
+        }
+        else 
+        {
+            mysqli_stmt_bind_param($stmt, 's', $name);
+            if(!mysqli_stmt_execute($stmt)) 
+            {
+                header("Location: index.php?R=2");
+            }
+            else 
+            {
+                header("Location: index.php?R=1");
+            }
+        }
+    ?>
+    
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
             include 'DBConnect.php';
             $groente = "groente";
             $bami = array($groente);
-            $query = "SELECT * FROM boodschappenlijst GROUP BY product";
+            $query = "SELECT product, count(product) FROM boodschappenlijst GROUP BY product";
             echo "<table class='border'>
                     <tr class='border'>
                             <th>Product</th>
@@ -21,34 +21,37 @@
                 }
                 else 
                 {
-                    mysqli_stmt_bind_result($stmt, $ID, $gerechtName);
+                    mysqli_stmt_bind_result($stmt, $gerechtName, $count);
                     mysqli_stmt_store_result($stmt);
                     while(mysqli_stmt_fetch($stmt)) 
                     {
                         echo "<tr>
                             <td>$gerechtName</td>
+                            <td>$count</td>
+                            <td><a href='deleteProduct.php?name=" . $gerechtName . "'>Verdwijder</a></td>
                         </tr>";
                     }
                 }
             }
-            echo "</table>";
             
-            //Yannik's delete dingetje, hij haalt de rijen uit de db en laat het zien in de dropdown
-            ?>
+            echo "</table>";
 
-            <form method="post" action="index.php">
-                <p>Verwijder een product</p>
-                <select name="product">
-                    <?php
-                    $sqly = mysqli_query($conn, "SELECT product FROM boodschappenlijst GROUP BY product");
-                    while ($row = mysqli_fetch_assoc($sqly))
-                    {
-                        ?>
-                        <option value="<?php echo $row["product"]; ?>"><?php echo $row['product']; ?></option>
-                        <?php } ?>
-                </select>
-                <input type="submit" name="delete" value="Verwijder product">
-            </form>
+            if(!empty($_GET["R"]))
+            {
+                if($_GET["R"] == 1)
+                {
+                    echo "<p>Product verdwijderd van het boodschappenlijstje</p>";
+                }
+                else if($_GET["R"] == 2)
+                {
+                    echo "<p>er is iets fout gegaan met het toevoegen van het product</p>";
+                }
+                else
+                {
+                    echo "<p>Alles is verdwijderd</p>";
+                }
+            }
+            ?>
             
              <!--Dit is voor het compleet leeghalen van de lijst  -->
              <br>
@@ -57,32 +60,7 @@
             </form>
             
             <?php
-            //Dit runt pas als de verwijder product knop is ingedrukt, en pakt automatisch de eerste db entry als product
-             if (isset($_POST['delete']))
-            {
-                $product = $_POST['product'];
-                $query2 = "DELETE FROM `boodschappenlijst` WHERE `product` = '$product'";
-                if (!$stmt = mysqli_prepare($conn, $query2))
-                {
-                    echo "failed to prepare statement";
-                }
-                else
-                {
-                    if(!mysqli_stmt_execute($stmt))
-                    {
-                        echo "failed to execute statement";
-                    }
-                    else
-                    {
-                        echo "<p> <font color=red> Product is verwijderd! </p>";
-                        echo "<meta http-equiv='refresh' content='1.0'>";
-                    }
-                }
-            } 
-            ?>
-            
-            <?php
-            //Dit zou dut moeten werken, maar werkt niet. Het gaat over de verwijder alles knop
+            //Alles verdwijderen
             if(isset($_POST['delAll']))
             {
                 $query2 = "TRUNCATE TABLE boodschappenlijst";
@@ -98,8 +76,7 @@
                     }
                     else
                     {
-                        echo "Het boodschappenlijstje wordt geleegd";
-                        echo "<meta http-equiv='refresh' content='.5'>";
+                        header("Location: index.php?R=3");
                     }
                 }
             } 
